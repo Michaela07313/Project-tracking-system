cb8dbf11c6618e24c31905e3e1f6e366a6db6dae
Michaela07313 <miminka07313@gmail.com>
2017-04-22 19:25:25 +0300

    add register form, registerGet controller, update the global layout, add index.hbs; update userSchema with type

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 let userSchema = mongoose.Schema({
-        userName: {type: String, required: true, unique: true},
+        email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
         firstName: {type: String, required: true},
         lastName: {type: String, required: true},
-        email: {type: String, required: true, unique: true},
+        type: {type: String, required: true},
         roles: [String],
         salt: {type: String, required: true}
 })
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,11 @@
 const homeController = require('./controllers/home')
+const userController = require('./controllers/user')
 
 module.exports = (app) => {
 
   app.get('/', homeController.index)
+
+  app.get('/users/register', userController.registerGet)
 
   app.all('*', (req, res) => {
     res.status(404)
